e529a56258b6f6f754323b065053092b7a0bf0f8
Álvaro Gallego Martínez <alvarok047@gmail.com>
2020-11-12 02:33:22 +0100

    1 - Añadido botón que modifica texto

---
+++ Proyecto/app/src/main/java/com/example/corecourse/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.corecourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView newText = (TextView) findViewById(R.id.main_text);
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                newText.setText("Ahora me siento más liberado");
+            }
+        });
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CORE-Android
 Repositorio para el desarrollo de aplicaciones Android impartido por Core Dumped
+
+### Hito 1
+- **Primer hito sobre android en java.** Añadido el botón que podrá cambiar el texto de la parte inferior de la pantalla.
