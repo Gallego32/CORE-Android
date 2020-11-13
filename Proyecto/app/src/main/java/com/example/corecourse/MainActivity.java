a18ad4e44284c771e808c693b25d9fe05e494ffb
Álvaro Gallego Martínez <alvarok047@gmail.com>
2020-11-13 12:43:07 +0100

    1 - Añadido Seekbar

--- Proyecto/app/src/main/java/com/example/corecourse/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/corecourse/MainActivity.java
@@ -5,22 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView newText, seekBarText, keepGoingText;
+    private SeekBar seekBar;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView newText = (TextView) findViewById(R.id.main_text);
+        newText = (TextView) findViewById(R.id.main_text);
 
-        final Button button = findViewById(R.id.button_id);
+        button = (Button) findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
                 newText.setText("Ahora me siento más liberado");
+            }
+        });
+
+        seekBarText = (TextView) findViewById(R.id.seekBarText);
+        keepGoingText = (TextView) findViewById(R.id.keepGoingText);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarText.setText("" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                keepGoingText.setText("Muy bien, así me gusta");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                if (seekBar.getProgress() != seekBar.getMax())
+                    keepGoingText.setText("¡¡Pero no pares!!");
+                else keepGoingText.setText("Ahí está bien :)");
             }
         });
     }
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Repositorio para el desarrollo de aplicaciones Android impartido por Core Dumped
 
 ### Hito 1
 - **Primer hito sobre android en java.** Añadido el botón que podrá cambiar el texto de la parte inferior de la pantalla.
+- Como tarea opcional he añadido una seekbar que modifica un valor y un texto.
